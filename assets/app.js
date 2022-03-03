f1013965354c79c395799ace71f278e0aecd1e3f
tungtt30 <tungchipsctn2@gmail.com>
2022-03-03 12:21:11 +0700

    chart

--- assets/app.js
+++ assets/app.js
@@ -5,28 +5,32 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, "slot_input");
 });
-
-
+const name_p = document.querySelectorAll(".name_p")
+const input_name = document.querySelector(".input_name")
+const input_score = document.querySelectorAll(".input_score")
+const btn_add = document.querySelector('.btn_add')
+const total = document.querySelectorAll('.total')
+const tbody = document.querySelector(".tbody")
+const btn_clr = document.querySelector(".btn_clr")
+let namearr = ['s1','s2','s3','s4','s5']
 let s = null
+let dataupdate = [0, 0, 0, 0, 0]
 const slot = document.querySelector(".slit")
 slot.addEventListener('change', (e) => {
     s = e.target.value - 1
+    input_name.value = ''
 })
-const name_p = document.querySelectorAll(".name_p")
-const input_name = document.querySelector(".input_name")
+
 input_name.addEventListener('input', () => {
     if (s == null) {
         alert('pick slot ??')
         input_name.value = ''
     }
     name_p[s].innerHTML = input_name.value
+    namearr[s] = input_name.value
+    chart.update()
 })
 
-const input_score = document.querySelectorAll(".input_score")
-const btn_add = document.querySelector('.btn_add')
-const total = document.querySelectorAll('.total')
-const tbody = document.querySelector(".tbody")
-const btn_clr = document.querySelector(".btn_clr")
 let p1 = []
 let p2 = []
 let p3 = []
@@ -40,15 +44,12 @@ const clearinput = () => {
     }
 }
 
-
-
 const push = () => {
     let p1score = Number(input_score[0].value)
     let p2score = Number(input_score[1].value)
     let p3score = Number(input_score[2].value)
     let p4score = Number(input_score[3].value)
     let p5score = Number(input_score[4].value)
-
 
     const pushe = () => {
         p1.push(p1score)
@@ -99,30 +100,25 @@ const push = () => {
     }
 }
 
-let tong1 = []
-let tong2 = []
-let tong3 = []
-let tong4 = []
-let tong5 = []
-
-
 const totem = () => {
 
     let a1 = p1.reduce((a, b) => (a + b))
     total[0].innerText = a1
-    tong1.push(a1)
+
     let a2 = p2.reduce((a, b) => (a + b))
-    tong2.push(a2)
+
     total[1].innerText = a2
     let a3 = p3.reduce((a, b) => (a + b))
-    tong3.push(a3)
+
     total[2].innerText = a3
     let a4 = p4.reduce((a, b) => (a + b))
-    tong4.push(a4)
+
     total[3].innerText = a4
     let a5 = p5.reduce((a, b) => (a + b))
-    tong5.push(a5)
+
     total[4].innerText = a5
+
+    dataupdate = [a1, a2, a3, a4, a5]
 }
 
 const key = "key"
@@ -130,9 +126,7 @@ let dock = {
     p1: p1, p2: p2, p3: p3, p4: p4, p5: p5
 }
 
-
 const stored = () => {
-
     window.localStorage.setItem(key, JSON.stringify(dock))
 }
 
@@ -147,64 +141,51 @@ const getStore = () => {
     p5 = item.p5
 }
 
+const data = {
+    labels: namearr
+    ,
+    datasets: [{
+        label: 'Sâm lốc analyze',
+        data: [0, 0, 0, 0, 0],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+        ],
+        borderColor: [
+           'red',
+           'green',
+           'violet',
+           'orange',
+           'blue'
+          ],
+          borderWidth: 1
+    }]
+};
+const chart = new Chart("myChart", {
+    type: "bar",
+    data: data,
+    options: {
+        legend: { display: false }
+    }
+});
+
 const render = () => {
     let ehe = tr.map(e => e)
     tbody.innerHTML = ehe.join('')
-
-    var xValues = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-
-    new Chart("myChart", {
-        type: "line",
-        data: {
-            labels: xValues,
-            datasets: [{
-                data: tong1,
-                borderColor: "red",
-                fill: false
-            },
-            {
-                data: tong2,
-                borderColor: "green",
-                fill: false
-            },
-            {
-                data: tong3,
-                borderColor: "violet",
-                fill: false
-            },
-            {
-                data: tong4,
-                borderColor: "orange",
-                fill: false
-            },
-            {
-                data: tong5,
-                borderColor: "blue",
-                fill: false
-            }]
-        },
-        options: {
-            legend: { display: false }
-        }
-    });
-
 }
 render()
-
-
-// stored()
-// getStore()
-
-
-
 
 btn_clr.addEventListener('click', () => {
     window.location.reload(true)
 })
 
 btn_add.addEventListener('click', () => {
-
     push()
+    data.datasets[0].data = dataupdate
+    chart.update()
     clearinput()
     render()
 })
