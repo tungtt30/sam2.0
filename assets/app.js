f6c937d59c8f7a6df071ac48dda16b1bc1a70e8d
tungtt30 <tungchipsctn2@gmail.com>
2022-03-04 16:30:01 +0700

    footer

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,4 @@
 
-
 //init selections
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
@@ -14,12 +13,9 @@ const tbody = document.querySelector(".tbody")
 const btn_clr = document.querySelector(".btn_clr")
 const btn_audio = document.querySelector(".btn_audio")
 const audio = document.querySelector(".audio")
-
 const res_add = document.querySelector(".add_responsive")
 const res_clr = document.querySelector(".clear_responsive")
-
 let isPlaying = false
-
 btn_audio.addEventListener('click', () => {
     if (isPlaying) {
         audio.pause()
@@ -30,13 +26,10 @@ btn_audio.addEventListener('click', () => {
         isPlaying = true
         return 1
     }
-
 })
 audio.addEventListener('ended', () => {
     isPlaying = false
-
 })
-
 let namearr = ['s1', 's2', 's3', 's4', 's5']
 let s = null
 let dataupdate = [0, 0, 0, 0, 0]
@@ -45,7 +38,6 @@ slot.addEventListener('change', (e) => {
     s = e.target.value - 1
     input_name.value = ''
 })
-
 input_name.addEventListener('input', () => {
     if (s == null) {
         alert('pick slot ??')
@@ -55,27 +47,23 @@ input_name.addEventListener('input', () => {
     namearr[s] = input_name.value
     chart.update()
 })
-
 let p1 = []
 let p2 = []
 let p3 = []
 let p4 = []
 let p5 = []
 const tr = []
-
 const clearinput = () => {
     for (let i = 0; i < input_score.length; i++) {
         input_score[i].value = null
     }
 }
-
 const push = () => {
     let p1score = Number(input_score[0].value)
     let p2score = Number(input_score[1].value)
     let p3score = Number(input_score[2].value)
     let p4score = Number(input_score[3].value)
     let p5score = Number(input_score[4].value)
-
     const pushe = () => {
         p1.push(p1score)
         p2.push(p2score)
@@ -124,38 +112,26 @@ const push = () => {
             alert("Nhập ngu")
     }
 }
-
 const totem = () => {
-
     let a1 = p1.reduce((a, b) => (a + b))
     total[0].innerText = a1
-
     let a2 = p2.reduce((a, b) => (a + b))
-
     total[1].innerText = a2
     let a3 = p3.reduce((a, b) => (a + b))
-
     total[2].innerText = a3
     let a4 = p4.reduce((a, b) => (a + b))
-
     total[3].innerText = a4
     let a5 = p5.reduce((a, b) => (a + b))
-
     total[4].innerText = a5
-
     dataupdate = [a1, a2, a3, a4, a5]
 }
-
 const key = "key"
 let dock = {
     p1: p1, p2: p2, p3: p3, p4: p4, p5: p5
 }
-
 const stored = () => {
     window.localStorage.setItem(key, JSON.stringify(dock))
 }
-
-
 const getStore = () => {
     var data = window.localStorage.getItem(key)
     var item = JSON.parse(data)
@@ -165,12 +141,10 @@ const getStore = () => {
     p4 = item.p4
     p5 = item.p5
 }
-
 const data = {
     labels: namearr
     ,
     datasets: [{
-        label: 'Sâm lốc analyze',
         data: [0, 0, 0, 0, 0],
         backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
@@ -196,13 +170,11 @@ const chart = new Chart("myChart", {
         legend: { display: false }
     }
 });
-
 const render = () => {
     let ehe = tr.map(e => e)
     tbody.innerHTML = ehe.join('')
 }
 render()
-
 btn_clr.addEventListener('click', () => {
     if (confirm('mất hết dữ liệu đấy !!') == true) {
         window.location.reload(true)
